8d7b8974dc282a2b23c95c5b9c192c8416317a3d
Sdpuchatt <sdpuchatt@gmail.com>
2024-08-12 16:32:44 -0300

    Incrementar decrementar carrito

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ function App() {
   const [data , setData] = useState(db)
   const [cart, setCart] = useState([])
 
+  const MAX_ITEMS = 5
+  const MIN_ITEMS = 1
+
   function addToCart(item){ 
     const itemExist = cart.findIndex((guitar) => guitar.id === item.id)
     if ( itemExist >= 0){ // existe en el carrtito
@@ -24,10 +27,44 @@ function App() {
     }
   }
 
+  function removeFromCart(id){
+    console.log('eliminando...', id)
+    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id ))
+  }
+
+  function increaseQuantity(id){
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity < MAX_ITEMS){
+        return{
+          ...item,
+          quantity: item.quantity + 1 
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  function decreaseQuantity(id){
+    const updateCart = cart.map(item => {
+        if(item.id === id && item.quantity > 1){
+        return{
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+    setCart(updateCart)
+  }
+
   return (
     <>
       <Header 
         cart = {cart}
+        removeFromCart = {removeFromCart}
+        increaseQuantity = {increaseQuantity}
+        decreaseQuantity = {decreaseQuantity}
       />
       <main className="container-xl mt-5">
           <h2 className="text-center">Nuestra Colección</h2>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import {useMemo} from 'react'
 
-export default function Header({cart}){
+export default function Header({cart ,removeFromCart, increaseQuantity, decreaseQuantity}){
     const isEmpty = useMemo(() => cart.length === 0 , [cart])
     const cartTotal = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.price),0) , [cart])
 
@@ -51,6 +51,7 @@ export default function Header({cart}){
                                                          <button
                                                              type="button"
                                                              className="btn btn-dark"
+                                                             onClick = {() => decreaseQuantity(guitar.id)}
                                                          >
                                                              -
                                                          </button>
@@ -58,6 +59,7 @@ export default function Header({cart}){
                                                          <button
                                                              type="button"
                                                              className="btn btn-dark"
+                                                             onClick={() => increaseQuantity(guitar.id)}
                                                          >
                                                              +
                                                          </button>
@@ -66,6 +68,7 @@ export default function Header({cart}){
                                                          <button
                                                              className="btn btn-danger"
                                                              type="button"
+                                                             onClick={() => removeFromCart(guitar.id)}
                                                          >
                                                              X
                                                          </button>
